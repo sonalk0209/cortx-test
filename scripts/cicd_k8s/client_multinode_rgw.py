794f951716c8a809817be9e35156ab8a6f4fd050
Sonal Kalbende <sonal.kalbende@seagate.com>
2022-03-22 17:26:03 +0530

    chnages from Akshay
    Signed-off-by: Sonal Kalbende <sonal.kalbende@seagate.com>

--- scripts/cicd_k8s/client_multinode_rgw.py
+++ scripts/cicd_k8s/client_multinode_rgw.py
@@ -126,6 +126,7 @@ def main():
     admin_passwd = os.getenv("ADMIN_PWD")
     node_obj = LogicalNode(hostname=master_node, username=username, password=args.password)
 
+    """
     resp = ext_lb.configure_nodeport_lb(node_obj, "eth1")
     if not resp[0]:
         print("Did not get expected response: {}".format(resp))
@@ -133,10 +134,22 @@ def main():
     port = resp[3]
     ext_port_ip = "{}:{}".format(ext_ip, port)
     print("External LB value, ip and port will be: {}".format(ext_port_ip))
+    """
+    resp = sysutils.execute_cmd(cmd=com_cmds.CMD_GET_IP_IFACE.format("eth1"))
+    ext_ip = resp[1].strip("'\\n'b'")
+    print("External LB IP: {}".format(ext_ip))
+    print("Creating haproxy.cfg for {} Node setup".format(args.master_node))
+    haproxy_cfg = config['default']['haproxy_config']
+    ext_lb.configure_haproxy_rgw_lb(
+        master_node, username=username, password=args.password, ext_ip=ext_ip)
+    with open(haproxy_cfg, 'r') as f_read:
+        print((45 * "*" + "haproxy.cfg" + 45 * "*" + "\n"))
+        print(f_read.read())
+        print((100 * "*" + "\n"))
 
     setupname = create_db_entry(master_node, username=username, password=args.password,
                                 admin_user=admin_user, admin_passwd=admin_passwd,
-                                ext_ip=ext_port_ip)
+                                ext_ip=ext_ip)
     print("target_name: {}".format(setupname))
     sysutils.execute_cmd(cmd="cp /root/secrets.json .")
     with open("/root/secrets.json", 'r') as file:

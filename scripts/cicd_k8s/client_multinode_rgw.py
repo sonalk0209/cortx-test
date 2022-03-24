869beffb0b70bfdc6f471222b4e713c67d644b06
Sonal Kalbende <sonal.kalbende@seagate.com>
2022-03-24 18:23:24 +0530

    minor
    Signed-off-by: Sonal Kalbende <sonal.kalbende@seagate.com>

--- scripts/cicd_k8s/client_multinode_rgw.py
+++ scripts/cicd_k8s/client_multinode_rgw.py
@@ -124,10 +124,10 @@ def main():
     username = "root"
     admin_user = os.getenv("ADMIN_USR")
     admin_passwd = os.getenv("ADMIN_PWD")
-    ext_node = os.getenv("EXT_NODE", "nodeport")
+    ext_node = os.getenv("SERVICE_TYPE", "NodePort")
     node_obj = LogicalNode(hostname=master_node, username=username, password=args.password)
 
-    if ext_node == "nodeport":
+    if ext_node == "NodePort":
         resp = ext_lb.configure_nodeport_lb(node_obj, "eth1")
         if not resp[0]:
             print("Did not get expected response: {}".format(resp))
